bb5a9198a3a2f89329a162631fba06a18aa0a742
ak <ak@go.lan>
2017-07-15 23:32:55 -0700

    alice middleware chain example

--- golang/middleware-chain/alice/alice.go
+++ golang/middleware-chain/alice/alice.go
@@ -1,3 +1,43 @@
 //TODO: GitHub - justinas/alice: Painless middleware chaining for Go
-https://github.com/justinas/alice
+//
+//
+//
+//
+
+package main
+
+import ("github.com/justinas/alice"
+	"net/http"
+	"fmt"
+	"time"
+)
+
+
+var middleware = alice.New(logger)
+
+func logger(next http.Handler)http.Handler {
+
+	return http.HandlerFunc(func (w http.ResponseWriter, r * http.Request){
+		fmt.Println("Logging begin")
+		t1:= time.Now()
+		next.ServeHTTP(w,r)
+		t2:=time.Since(t1)
+		fmt.Println("request duration", t2)
+		fmt.Println("Logging end")
+	})
+
+}
+
+
+func welcomeHandler(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("hello world"))
+}
+
+func main(){
+
+	http.Handle("/", middleware.ThenFunc(welcomeHandler))
+
+	http.ListenAndServe(":8080", nil)
+
+}
 
