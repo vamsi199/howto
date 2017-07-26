a07fe25bec51dd52c5f384a56da899d0e7010bce
Srinivasa R Muly <msrinivasareddy@gmail.com>
2017-07-26 16:06:13 -0700

    Update autoinc-TODO.go

--- golang/db/pg/orm/xorm/autoinc-TODO.go
+++ golang/db/pg/orm/xorm/autoinc-TODO.go
@@ -1,1 +1,2 @@
-
+//TODO: using xorm to make the autoincrement work. i.e even if the Id field is not passed, xorm should autoincrement and save the field to the Id field in dataabse,. 
+// also there should be a way to retrieve the created Id field, so that the calling function know which Id got inserted.
