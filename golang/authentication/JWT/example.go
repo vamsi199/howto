1102aa7b66de2b1277c70f10eccb7b31f8f0d5bb
muly <muly@go.lan>
2017-07-26 01:03:27 -0700

    JWT example completed

--- golang/authentication/JWT/example.go
+++ golang/authentication/JWT/example.go
@@ -9,11 +9,12 @@ import (
 	"time"
 
 	"github.com/justinas/alice"
+	"net/url"
 )
 
 var mySigningKey = []byte("secret")
 
-var middleware = alice.New(ValidateToken)
+var middleware = alice.New(SetHeader, ValidateToken)
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
 
@@ -26,41 +27,76 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	claims := token.Claims.(jwt.MapClaims)
 	claims["admin"] = true
 	//claims["name"] = "??"
-	claims["exp"] = time.Now().Add(time.Minute * 1).Unix()
+	claims["exp"] = time.Now().Add(time.Minute * 10).Unix()
 
 	/* Sign the token with our secret */
 	tokenString, _ := token.SignedString(mySigningKey)
 
-	//TODO return token in body or header?
-	w.Write([]byte(tokenString))
+	//TODO return token in body or header? or in other mode so that the subsequent requests from the same browser will save the token
+	//w.Write([]byte(tokenString))
+	//w.Header().Add("Authorization","Bearer "+tokenString)
+
+	values := url.Values{}
+	values.Add("token", tokenString)
+
+	redirectRequestUrl := fmt.Sprintf("localhost:8080/hello?%s",
+		values.Encode())
+
+	http.Redirect(w, r, redirectRequestUrl, http.StatusOK)
+	//w.Write([]byte("authorized"))
+}
+
+func SetHeader(next http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("### SetHeader begin", r.URL)
+		//TODO: is this the only way to read the query parameters?
+		vars, err := url.ParseQuery(r.URL.RawQuery)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		}
+
+		if val, exists := vars["token"]; exists {
+			r.Header.Add("Authorization","Bearer "+val[0])
+		}
+		next.ServeHTTP(w, r)
+		fmt.Println("### SetHeader end")
+	})
 }
 
 //func ValidateTokenMiddleware(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 func ValidateToken(next http.Handler) http.Handler {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("### ValidateToken begin", r.URL)
 		token, err := request.ParseFromRequest(r, request.AuthorizationHeaderExtractor,
 			func(token *jwt.Token) (interface{}, error) {
 				return mySigningKey, nil
 			})
 		if err != nil {
-			w.WriteHeader(http.StatusUnauthorized)
-			fmt.Fprint(w, token.Raw+"\nUnauthorized access to this resource\n"+err.Error())
+			fmt.Println("### ValidateToken error: ", err)
+			//w.WriteHeader(http.StatusUnauthorized)
+			//fmt.Fprint(w, "\nUnauthorized access to this resource\n"+err.Error())
 			http.Redirect(w, r, "localhost:8080/login", http.StatusUnauthorized)
 			return
 		}
 		if !token.Valid {
 			w.WriteHeader(http.StatusUnauthorized)
-			fmt.Fprint(w, "Token is not valid")
+			fmt.Println("### ValidateToken error: Token is not valid")
 			http.Redirect(w, r, "localhost:8080/login", http.StatusUnauthorized)
 			return
 		}
 		next.ServeHTTP(w, r)
+
+		fmt.Println("### ValidateToken end")
 	})
 }
 
 func welcomeHandler(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("### welcomeHandler begin")
 	w.Write([]byte("hello world"))
+	fmt.Println("### welcomeHandler end")
 }
 
 func main() {
