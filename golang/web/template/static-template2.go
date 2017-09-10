3b68ebdeb7f52560b866f1be6660020fe7a74040
Praveen <praveen.goprog@gmail.com>
2017-09-10 00:54:45 -0700

    parse static template from a file and execute

--- golang/web/template/static-template2.go
+++ golang/web/template/static-template2.go
@@ -1,1 +1,19 @@
 //TODO: simple static template read from a file, parse and execute it
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+	"text/template"
+)
+
+func main() {
+	myr := mux.NewRouter()
+	myr.HandleFunc("/", tmplHandler)
+	http.ListenAndServe(":8080", myr)
+}
+
+func tmplHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("stpl2.gohtml"))
+	tmpl.Execute(w,&tmpl)
+}
