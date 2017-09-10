d25b3d17341b227aa3c75e51f80386af7594d6d0
Praveen <praveen.goprog@gmail.com>
2017-09-10 00:11:03 -0700

    code for executing static template by simple parse

--- golang/web/template/static-template1.go
+++ golang/web/template/static-template1.go
@@ -1,1 +1,25 @@
 //TODO: simple static template that just reads the temaplate from a constant parses and executes it
+package main
+
+import (
+	"os"
+	"text/template"
+)
+
+func main() {
+	stmpl := `<!DOCTYEP=html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>static-template1</title>
+<body>
+Hello World from Static Template1
+</body>
+</html>
+`
+	tmpl, err := template.New("static").Parse(stmpl)
+	if err != nil {
+		panic(err)
+	}
+	tmpl.Execute(os.Stdout, stmpl)
+}
