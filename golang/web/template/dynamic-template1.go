0d9cdec8e2bd15945a6b4de3fff5efa9b482814b
Praveen <praveen.goprog@gmail.com>
2017-09-11 06:16:44 -0700

    dynamic template using a struct and render

--- golang/web/template/dynamic-template1.go
+++ golang/web/template/dynamic-template1.go
@@ -1,1 +1,26 @@
 // dynamic template that uses a struct to inject the data and render it.
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+	"text/template"
+)
+
+type people struct {
+	Fname string
+	Mname  string
+	Lname string
+	Age int
+}
+
+func main() {
+	myr := mux.NewRouter()
+	myr.HandleFunc("/", tmplHandler)
+	http.ListenAndServe(":8080", myr)
+}
+func tmplHandler(w http.ResponseWriter, r *http.Request) {
+	p := people{"Praveen", "Kumar","K",37}
+	tmpl := template.Must(template.ParseFiles("dtpl.gohtml"))
+	tmpl.Execute(w, p)
+}
