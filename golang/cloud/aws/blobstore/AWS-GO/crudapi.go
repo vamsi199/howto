cf84b963a6f7de1b100f098883c05b39cbd6db06
vamsibottu <vamsibottu@gmail.com>
2017-09-15 17:00:54 -0400

    Cleanups

--- golang/cloud/aws/blobstore/AWS-GO/crudapi.go
+++ golang/cloud/aws/blobstore/AWS-GO/crudapi.go
@@ -11,10 +11,12 @@ import (
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+  "github.com/aws/aws-sdk-go/aws/awserr"
+
 )
 
 func main() {
-	aws_access_key_id := "(AKIAJFLY5AM7GO7R****)"                       //please Enter the credentials
+	aws_access_key_id := "AKIAJFLY5AM7GO7R****"                       //please Enter the credentials
 	aws_secret_access_key := "apQ9pVjPO7QezyBveFLPiaE2O47+eLnY5e4*****" //please enter the credentials
 	token := ""
 	creds := credentials.NewStaticCredentials(aws_access_key_id, aws_secret_access_key, token)
@@ -49,5 +51,76 @@ func main() {
 	if err != nil {
 		fmt.Printf("bad response: %s", err)
 	}
-	fmt.Printf("response %s", awsutil.StringValue(resp))
+	fmt.Printf("response %s",awsutil.StringValue(resp))
+
+  //Read or Download object //TODO add new func
+  input := &s3.GetObjectInput{
+      Bucket: aws.String("crudapi"),
+      Key:    aws.String(path),
+  }
+
+  result, err := svc.GetObject(input)
+  if err != nil {
+      if aerr, ok := err.(awserr.Error); ok {
+          switch aerr.Code() {
+          case s3.ErrCodeNoSuchKey:
+              fmt.Println(s3.ErrCodeNoSuchKey, aerr.Error())
+          default:
+              fmt.Println(aerr.Error())
+          }
+      } else {
+
+          fmt.Println(err.Error())
+      }
+      return
+  }
+
+  fmt.Println("\n Sucessfully get the object", result)
+
+//List all objects present input Bucket //TODO need to add route
+ listinput := &s3.ListObjectsInput{
+      Bucket:  aws.String("crudapi"),
+      MaxKeys: aws.Int64(2),
+  }
+
+  list, err := svc.ListObjects(listinput)
+  if err != nil {
+      if aerr, ok := err.(awserr.Error); ok {
+          switch aerr.Code() {
+          case s3.ErrCodeNoSuchBucket:
+              fmt.Println(s3.ErrCodeNoSuchBucket, aerr.Error())
+          default:
+              fmt.Println(aerr.Error())
+          }
+      } else {
+
+          fmt.Println(err.Error())
+      }
+      return
+  }
+
+  fmt.Println("List of an objects", list)
+
+//Delete objects present in Input //TODO need to add route
+  deleteinput := &s3.DeleteObjectInput{
+    Bucket: aws.String("crudapi"),
+    Key:    aws.String(path),
+}
+
+deletedresult, err := svc.DeleteObject(deleteinput)
+if err != nil {
+    if aerr, ok := err.(awserr.Error); ok {
+        switch aerr.Code() {
+        default:
+            fmt.Println(aerr.Error())
+        }
+    } else {
+        fmt.Println(err.Error())
+    }
+    return
+}
+
+fmt.Println("object deleted sucessfully", deleteinput, deletedresult)
+
+
 }
