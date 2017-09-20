8172c2c3a8ea4a042437f567eb237d85d6240249
muly <msrinivasareddy@gmail.com>
2017-09-19 20:23:16 -0700

    s3 crud wip

--- golang/cloud/aws/s3/s3crud2TODO.go
+++ golang/cloud/aws/s3/s3crud2TODO.go
@@ -6,22 +6,24 @@ import (
 	"errors"
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"os"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 )
 
 var sess *session.Session
-var bkt *string
+var s3Client *s3.S3
 
 func init() {
-	// configuration fields //TODO: crete single object to store all the required config fields for s3. i.e the list below
-	accessId := os.Getenv("s3accessid")
-	accessKey := os.Getenv("secretaccesskey")
+	// configuration fields //TODO: create single object to store all the required config fields for s3. i.e the list below
+	accessId := os.Getenv("s3accessid") //TODO: need to change the credentialsenv variables to AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+	accessKey := os.Getenv("s3accesskey")
 	token := ""
-	region := "us-east-1"
-	bkt = aws.String("jenkins19")
+	region := "us-west-1"
+
+	//fmt.Println("access ID, Key: ", accessId, accessKey)
 
 	cred := credentials.NewStaticCredentials(accessId, accessKey, token)
 	_, err := cred.Get()
@@ -35,33 +37,48 @@ func init() {
 
 	sess = session.Must(session.NewSession(conf))
 
+
+
+
 }
 
 func main() {
 
-	key, err := upload("file")
+	//filePath := "s3crud2TODO.go"
+	bkt := "muly123"
+
+/*	err := createBkt(bkt)
+	if err != nil {
+		fmt.Println("bucket create error:", err)
+	}
+	fmt.Printf("bucket %v created sucessfully\n", bkt)*/
+
+	filePath := "s3crud2TODO.go"
+	key, err := upload(filePath, bkt)
 	if err != nil {
 		fmt.Println("upload error:", err)
+		return
 	}
-	fmt.Printf("uploaded %v sucessfully", key)
+	fmt.Printf("uploaded %v sucessfully\n", key)
 
-	keys, err := list()
+	keys, err := list(bkt)
 	if err != nil {
 		fmt.Println("list error:", err)
+		return
 	}
-	fmt.Printf("list sucessful %v", keys)
+	fmt.Printf("list sucessful %v\n", keys)
 
 	for _, key := range keys {
-		if err = download(key); err != nil {
-			fmt.Printf("download %v error: %v", key, err)
+		if err = download(key, bkt); err != nil {
+			fmt.Printf("download %v error: %v\n", key, err)
 			continue
 		}
 		fmt.Println("download sucessfull:", key)
 	}
 
 	for _, key := range keys {
-		if err = delete(key); err != nil {
-			fmt.Printf("delete %v error: %v", key, err)
+		if err = delete(key, bkt); err != nil {
+			fmt.Printf("delete %v error: %v\n", key, err)
 			continue
 		}
 		fmt.Println("delete sucessfull:", key)
@@ -69,34 +86,47 @@ func main() {
 
 }
 
-func upload(filePath string) (key string, err error) {
+/*func createBkt(bkt string)error{
+
+	input := &s3.CreateBucketInput{Bucket: aws.String(bkt)}
+	_, err:= s3Client.CreateBucket(input)
+	if err != nil{
+		return err
+	}
+
+	return nil
+}*/
+
+func upload(filePath string, bkt string) (key string, err error) {
 	upload := s3manager.NewUploader(sess)
 	f, err := os.Open(filePath)
 	if err != nil {
+		fmt.Println("error to open file")
 		return "", err
 	}
 	defer f.Close()
 
 	key = filePath
-	uploadInput := s3manager.UploadInput{Bucket: bkt, Body: f, Key: aws.String(key)}
+	uploadInput := s3manager.UploadInput{Bucket: aws.String(bkt), Body: f, Key: aws.String(key)}
 	_, err = upload.Upload(&uploadInput)
 	if err != nil {
+		fmt.Println("error uploading file")
 		return "", err
 	}
 	return key, nil
 }
 
-func download(key string) error {
+func download(key string, bkt string) error {
 
 	return errors.New("not implemented")
 }
 
-func delete(key string) error {
+func delete(key string, bkt string) error {
 
 	return errors.New("not implemented")
 }
 
-func list() (keys []string, err error) {
+func list(bkt string) (keys []string, err error) {
 
 	return nil, errors.New("not implemented")
 }
