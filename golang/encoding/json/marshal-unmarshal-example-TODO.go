df9ed0581c9ba97d19c87c28daa59886905859a7
muly <muly@srinivasas-mbp.lan>
2017-10-01 23:11:40 -0700

    json marshal unmarshal examples

--- golang/encoding/json/marshal-unmarshal-example-TODO.go
+++ golang/encoding/json/marshal-unmarshal-example-TODO.go
@@ -1,1 +1,34 @@
 // example program to demonstrate the use of `Marshal`, `Unmarshal` functions of `encoding/json` package
+//// Marshal: struct to json string
+//// Unmarshal: json string to struct
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type dog struct {
+	Color string `json:"color"`
+	Breed string `json:"breed"`
+	Age   int    `json:"age"`
+}
+
+func main() {
+	marshalExample()
+	unmarshalExample()
+}
+
+func marshalExample() {
+	d := dog{Color:"brown", Breed:"German Shepherd", Age:5}
+	b, _ :=json.Marshal(&d)
+	fmt.Println(string(b))
+}
+
+func unmarshalExample(){
+	jsonStr := `{"color":"brown","breed":"German Shepherd","age":5}`
+	d:=dog{}
+	json.Unmarshal([]byte(jsonStr), &d)
+	fmt.Println(d)
+}
