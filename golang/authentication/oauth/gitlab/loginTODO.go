88c2ea8f22c99d0de3ec09cc38b024a7de71459e
Sai Kode <saikirankode@gmail.com>
2017-10-11 12:26:16 -0400

    gitlab_oauth

--- golang/authentication/oauth/gitlab/loginTODO.go
+++ golang/authentication/oauth/gitlab/loginTODO.go
@@ -1,1 +1,38 @@
+package gitlab
+
+import (
+	"fmt"
+	"github.com/google/uuid"
+	"net/http"
+	"net/url"
+)
+
+func HandleLogin(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("&&& handleGithubLogin begin")
+	fmt.Println("app url:", r.Host)
+
+	id := uuid.New()
+	fmt.Println("id:", id)
+	//ctx := r.Context()
+	//TODO: get session from context
+
+	//redirect_uri := "http://localhost:8080/callback"
+	redirect_uri := "http://" + r.Host + "/callback"
+	fmt.Println("redirect_uri:", redirect_uri)
+
+	values := url.Values{}
+	values.Add("client_id", "385fa828028158f53e8eb52bfa3112830d4a86d3771a66de2d1d747f9960b701")
+	values.Add("redirect_uri", redirect_uri)
+	values.Add("response_type", "code")
+	values.Add("state", id.String())
+
+	redirectRequestUrl := fmt.Sprintf("https://gitlab.com/oauth/authorize?%s",
+		values.Encode())
+
+	fmt.Println("redirectRequestUrl:", redirectRequestUrl)
+
+	//TODO: save session back to context after saving the id.String()  to a field in session.State
+
+	http.Redirect(w, r, redirectRequestUrl, 302)
+}
 
--- golang/authentication/oauth/main.go
+++ golang/authentication/oauth/main.go
@@ -1,13 +1,14 @@
 package main
 
-import ("net/http"
-	"io"
+import (
 	"github.com/muly/howto/golang/authentication/oauth/fb"
 	"github.com/muly/howto/golang/authentication/oauth/github"
-	"github.com/muly/howto/golang/authentication/oauth/linkedin"
+	"github.com/muly/howto/golang/authentication/oauth/gitlab"
 	"github.com/muly/howto/golang/authentication/oauth/google"
+	"github.com/muly/howto/golang/authentication/oauth/linkedin"
+	"io"
+	"net/http"
 )
-
 
 const loginhtml = `<!DOCTYPE html>
 <html>
@@ -15,14 +16,17 @@ const loginhtml = `<!DOCTYPE html>
 <body>
 <p><a href="/facebooklogin">LOGIN WITH FACEBOOK </a></p>
 <p><a href="/githublogin">LOGIN WITH GITHUB</a></p>
+<p><a href="/gitlablogin">LOGIN WITH GITLAB</a></p>
 <p><a href="/googlelogin">LOGIN WITH GOOGLE</a></p>
 <a href="/linkedinlogin">LOGIN WITH LINKEDIN</a>
 </body>
 </html>
 `
+
 func main() {
 	http.HandleFunc("/", handleindex)
 	http.HandleFunc("/githublogin", github.HandleLogin)
+	http.HandleFunc("/gitlablogin", gitlab.HandleLogin)
 	http.HandleFunc("/facebooklogin", fb.HandleLogin)
 	http.HandleFunc("/linkedinlogin", linkedin.HandleLogin)
 	http.HandleFunc("/googlelogin", google.HandleLogin)
