06acecb7d0e8f8ec072fca6d1241f29e5140eace
Srinivasa R Muly <msrinivasareddy@gmail.com>
2017-10-14 12:11:27 -0700

    Update httpclientTODO.go

--- golang/web/client/httpclientTODO.go
+++ golang/web/client/httpclientTODO.go
@@ -1,3 +1,6 @@
 // write HTTP client to make a get request to an existing web service
 
 // something like client.Do
+
+
+// note: https://medium.com/@nate510/don-t-use-go-s-default-http-client-4804cb19f779
