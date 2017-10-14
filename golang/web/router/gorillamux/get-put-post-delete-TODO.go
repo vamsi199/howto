d8b529fc26d5be5aa4da5ac9c9ae102db6abe7f6
Praveen <praveen.goprog@gmail.com>
2017-10-14 12:04:56 -0700

    naming handlers in gorillamux

--- golang/web/router/gorillamux/get-put-post-delete-TODO.go
+++ golang/web/router/gorillamux/get-put-post-delete-TODO.go
@@ -10,29 +10,29 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type custm struct {
+type customer struct {
 	Id   int    `json:"id"`
 	Name string `json:"name"`
 }
 
-var Custms []custm
+var Customers []customer
 
 func main() {
 	//add records to structs
-	Custms = append(Custms, custm{1, "Bobby"})
-	Custms = append(Custms, custm{2, "Robin"})
+	Customers = append(Customers, customer{1, "Bobby"})
+	Customers = append(Customers, customer{2, "Robin"})
 
 	router := mux.NewRouter()
 	//customer api
-	router.HandleFunc("/customers", getCustomers).Methods("GET")                //gets all customers
-	router.HandleFunc("/customers/{id}", getCustomer).Methods("GET")            //gets one customer
-	router.HandleFunc("/customers/{id}/{name}", editCustomer).Methods("PUT")    //gets one customer
-	router.HandleFunc("/customers/{id}/{name}", createCustomer).Methods("POST") //creates new customer
-	router.HandleFunc("/customers/{id}", deleteCustomer).Methods("DELETE")      //deletes a customer
+	router.HandleFunc("/customers", customerHandler).Methods("GET")                    //gets all customers
+	router.HandleFunc("/customers/{id}", getCustomerHandler).Methods("GET")            //gets one customer
+	router.HandleFunc("/customers/{id}/{name}", editCustomerHandler).Methods("PUT")    //gets one customer
+	router.HandleFunc("/customers/{id}/{name}", createCustomerHandler).Methods("POST") //creates new customer
+	router.HandleFunc("/customers/{id}", deleteCustomerHandler).Methods("DELETE")      //deletes a customer
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
-func jMarshal(w http.ResponseWriter, r *http.Request, res []custm) {
+func jMarshal(w http.ResponseWriter, r *http.Request, res []customer) {
 	jResult, err := json.Marshal(res)
 	if err != nil {
 		http.Error(w, "marshal Error...:"+err.Error(), http.StatusInternalServerError)
@@ -40,70 +40,70 @@ func jMarshal(w http.ResponseWriter, r *http.Request, res []custm) {
 	}
 	w.Write(jResult)
 }
-func getCustomers(w http.ResponseWriter, r *http.Request) {
+func customerHandler(w http.ResponseWriter, r *http.Request) {
 	//fetch all customers
-	jResult, err := json.Marshal(Custms)
+	jResult, err := json.Marshal(Customers)
 	if err != nil {
 		http.Error(w, "marhsal error", http.StatusInternalServerError)
 	}
 	w.Write(jResult)
 }
-func getCustomer(w http.ResponseWriter, r *http.Request) {
+func getCustomerHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	jResult := []custm{} //need to empty the slice before appending query results
-	for _, c := range Custms {
+	jResult := []customer{} //need to empty the slice before appending query results
+	for _, c := range Customers {
 		if strconv.Itoa(c.Id) == vars["id"] {
 			jResult = append(jResult, c)
 		}
 	}
 	jMarshal(w, r, jResult) //gives key and values whereas fmt.Fprint gives only values
 }
-func editCustomer(w http.ResponseWriter, r *http.Request) {
+func editCustomerHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	nId, err := strconv.Atoi(vars["id"])
 	if err != nil {
 		fmt.Fprint(w, "type conversion error...")
 		return
 	}
-	for i, j := range Custms {
+	for i, j := range Customers {
 		if j.Id == nId {
-			Custms[i].Name = vars["name"]
+			Customers[i].Name = vars["name"]
 		}
 	}
-	jMarshal(w, r, Custms)
+	jMarshal(w, r, Customers)
 
 }
-func createCustomer(w http.ResponseWriter, r *http.Request) {
+func createCustomerHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	nId, err := strconv.Atoi(vars["id"])
 	if err != nil {
 		fmt.Fprintf(w, "type conversion error", http.StatusInternalServerError)
 	}
 
-	for _, i := range Custms {
+	for _, i := range Customers {
 		if i.Id == nId {
 			http.Error(w, "customer already exists", http.StatusConflict)
 			return
 		}
 	}
-	var cus custm
+	var cus customer
 	cus.Id = nId
 	cus.Name = vars["name"] //did this by split(/) of vars
-	Custms = append(Custms, cus)
-	jMarshal(w, r, Custms)
+	Customers = append(Customers, cus)
+	jMarshal(w, r, Customers)
 }
-func deleteCustomer(w http.ResponseWriter, r *http.Request) {
+func deleteCustomerHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	nId, err := strconv.Atoi(vars["id"])
 	if err != nil {
 		fmt.Fprintf(w, "conversion error", http.StatusInternalServerError)
 	}
-	for i, j := range Custms { //need how this worked ?
+	for i, j := range Customers { //need how this worked ?
 		if j.Id == nId {
-			Custms = append(Custms[:i], Custms[i+1:]...)
-			//delete(Custms,nId)//helpful only if custms is a map of slice
+			Customers = append(Customers[:i], Customers[i+1:]...)
+			//delete(Customers,nId)//helpful only if custms is a map of slice
 			break
 		}
 	}
-	jMarshal(w, r, Custms)
+	jMarshal(w, r, Customers)
 }
