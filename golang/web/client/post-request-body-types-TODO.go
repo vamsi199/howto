e72ff032d89b66e284205b7c4bd4bd47f74a02a5
Srinivasa R Muly <msrinivasareddy@gmail.com>
2017-10-14 23:38:26 -0700

    Create post-request-body-types-TODO.go

---
+++ golang/web/client/post-request-body-types-TODO.go
@@ -0,0 +1,6 @@
+send the client request with data in body. give examples of sending data in differnt formats listed below:
+1) form data
+2) x-www-form-urlencoded
+3) raw data: json or any plain text formats
+4) binary
+5) multipart file attachment
