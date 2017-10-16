ca28326c35bcc560035201722d201362d64554e4
Praveen <praveen.goprog@gmail.com>
2017-10-16 10:23:52 -0700

    client-http-client-get request

--- golang/web/client/httpclientTODO.go
+++ golang/web/client/httpclientTODO.go
@@ -2,5 +2,34 @@
 
 // something like client.Do
 
-
 // note: https://medium.com/@nate510/don-t-use-go-s-default-http-client-4804cb19f779
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+func main() {
+
+	v := url.Values{}
+	v.Add("q", "golang")
+	reqURL := fmt.Sprintf("https://google.com/search?%s", v.Encode())
+
+	r, _ := http.NewRequest("GET", reqURL, nil)
+	fmt.Println(r)
+
+
+	client := http.Client{}
+	resp, _ := client.Do(r)
+	fmt.Println(resp.Status)
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(b))
+
+}
