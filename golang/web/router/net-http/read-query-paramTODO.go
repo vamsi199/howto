6350f9f29ca01e402554fbb203b6611108bf8f04
Srinivasa R Muly <msrinivasareddy@gmail.com>
2017-10-23 13:26:32 -0700

    Update read-query-paramTODO.go

--- golang/web/router/net-http/read-query-paramTODO.go
+++ golang/web/router/net-http/read-query-paramTODO.go
@@ -1,1 +1,20 @@
-//example to demonstrate reading query parameters using net/http library 
+//example to demonstrate reading query parameters using net/http library. example: https://localhost:8080/customer?country=usa
+//TODO: need to run and test
+package main
+
+import (
+  "net/http"
+)
+
+func HandleActivitiesGet(w http.ResponseWriter, r *http.Request) {
+  vars, err := url.ParseQuery(r.URL.RawQuery)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
+  country := ""
+	if val, exists := vars["country"]; exists {
+		country = val[0]
+	}
+  fmt.Fprintln(w, country)
+}
