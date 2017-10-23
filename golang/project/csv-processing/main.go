1de37fbb3726523ab53b1160b1f743b11bc470df
muly <muly@srinivasas-mbp.lan>
2017-10-23 08:58:11 -0700

    struct changes for DRY

--- golang/project/csv-processing/main.go
+++ golang/project/csv-processing/main.go
@@ -6,14 +6,21 @@ import (
 	"os"
 )
 
+type Common struct {
+	Road_ID     int
+	Start_Chain float32
+	End_Chain   float32
+	Admin_Unit  int
+}
+type OutputCway struct {
+	Cway_Type  string
+	Cway_Width int
+}
+
 type dataCway struct {
-	Road_ID      int
-	Start_Chain  float32
-	End_Chain    float32
-	Admin_Unit   int
+	Common
 	Terrain_Type string
-	Cway_Type    string
-	Cway_Width   int
+	OutputCway
 	Embakment    string
 	Submergent   string
 	Right_of_Way string
@@ -22,11 +29,7 @@ type dataCway struct {
 	File_Name    string
 }
 
-type dataDsin struct {
-	Road_ID      int
-	Start_Chain  float32
-	End_Chain    float32
-	Admin_Unit   int
+type OutputDsin struct {
 	WSC_Extent   string
 	WSC_Severity string
 	EB_Extent    string
@@ -35,16 +38,22 @@ type dataDsin struct {
 	RV_Severity  string
 	PH_Extent    string
 	PH_Severity  string
-	Survey_Date  string
-	File_Name    string
+}
+
+type dataDsin struct {
+	Common
+	OutputDsin
+	Survey_Date string
+	File_Name   string
+}
+
+type OutputRoug struct {
+	IRI_Average string
 }
 
 type dataRoug struct {
-	Road_ID        int
-	Start_Chain    float32
-	End_Chain      float32
-	Admin_Unit     int
-	IRI_Average    string
+	Common
+	OutputRoug
 	Survey_Date    string
 	Year_of_Survey string
 	Notes          string
@@ -52,26 +61,14 @@ type dataRoug struct {
 }
 
 type dataOutput struct { // TODO: need some changes to this struct fields
-	dataCway
-	dataDsin
-	dataRoug
-	Road_ID      int
-	Start_Chain  float32
-	End_Chain    float32
-	Admin_Unit   int
-	Cway_Type    string
-	Cway_Width   int
-	WSC_Extent   string
-	WSC_Severity string
-	EB_Extent    string
-	EB_Severity  string
-	RV_Extent    string
-	RV_Severity  string
-	PH_Extent    string
+	Common
+	OutputCway
+	OutputDsin
+	OutputRoug
 }
 
 func readCway() []dataCway {
-	fileCWAY, err := os.Open("data/CWAY.csv")
+	fileCWAY, err := os.Open("/Users/muly/go/src/github.com/muly/howto/golang/project/csv-processing/data/CWAY.csv")
 	if err != nil {
 		panic(err)
 	}
@@ -87,7 +84,7 @@ func readCway() []dataCway {
 }
 
 func readDsin() []dataDsin {
-	fileDSIN, err := os.Open("data/DSIN.csv")
+	fileDSIN, err := os.Open("/Users/muly/go/src/github.com/muly/howto/golang/project/csv-processing/data/DSIN.csv")
 	if err != nil {
 		panic(err)
 	}
@@ -103,7 +100,7 @@ func readDsin() []dataDsin {
 }
 
 func readRoug() []dataRoug {
-	fileROUG, err := os.Open("data/ROUG.csv")
+	fileROUG, err := os.Open("/Users/muly/go/src/github.com/muly/howto/golang/project/csv-processing/data/ROUG.csv")
 	if err != nil {
 		panic(err)
 	}
@@ -122,13 +119,41 @@ func main() {
 
 	//read all the 3 files
 	cway := readCway()
-	for _, c := range cway {
-		fmt.Println(c)
-	}
-	//readDsin()
-	//readRoug()
+	dsin := readDsin()
+	roug := readRoug()
+
+	//for _, c := range cway {
+	//	fmt.Println(c)
+	//}
+	//for _, c := range dsin {
+	//	fmt.Println(c)
+	//}
+	//for _, c := range roug {
+	//	fmt.Println(c)
+	//}
+
 
 	// merge them into single dataset, get only the necessary fields from each of the structs
+	temp := make([]dataOutput, 0, len(cway)+len(dsin)+len(roug))
+	for _, c := range cway {
+		t := dataOutput{Common:c.Common}
+		//t.OutputCway = c.OutputCway
+		temp = append(temp, t)
+	}
+	for _, c := range dsin {
+		t := dataOutput{Common:c.Common}
+		temp = append(temp, t)
+	}
+	for _, c := range roug {
+		t := dataOutput{Common:c.Common}
+		temp = append(temp, t)
+	}
+
+
+	for _, c := range temp{
+		fmt.Println(c)
+	}
+
 
 	// for each Road_ID
 
