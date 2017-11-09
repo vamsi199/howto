645f5f458831c84c176a59e42bd257f037caf851
Srinivasa R Muly <msrinivasareddy@gmail.com>
2017-11-08 19:15:07 -0800

    Update post-request-body-types-TODO.go

--- golang/web/server/post-request-body-types-TODO.go
+++ golang/web/server/post-request-body-types-TODO.go
@@ -1,1 +1,6 @@
-
+accept the request to server with data in body in various ways listed below listed below:
+1) form data (types: text, file)
+2) x-www-form-urlencoded
+3) raw data: json or any plain text formats
+4) binary
+5) multipart file attachment?????
