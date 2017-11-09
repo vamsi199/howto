34a359eea74fee66fed3bef9823a66a078b90910
muly <msrinivasareddy@gmail.com>
2017-11-08 19:37:12 -0800

    C:/Program Files/Git/post-request-body-types wip

--- golang/web/server/post-request-body-types-TODO.go
+++ golang/web/server/post-request-body-types-TODO.go
@@ -1,6 +1,22 @@
-accept the request to server with data in body in various ways listed below:
-1) form data (types: text, file)
-2) x-www-form-urlencoded
-3) raw data: json or any plain text formats
-4) binary
-5) multipart file attachment?????
+//accept the request to server with data in body in various ways listed below:
+//1) form data (types: text, file)
+//2) x-www-form-urlencoded
+//3) raw data: json or any plain text formats
+//4) binary
+//5) multipart file attachment?????
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/formdatafile", handlerFormDataFileType)
+	http.ListenAndServe(":8080", nil)
+}
+
+//1b) form data file type example
+func handlerFormDataFileType(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "file received")
+}
