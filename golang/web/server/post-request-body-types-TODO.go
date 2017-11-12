8abcb5dcf5f5551432ed595b63c098925ffbbdc5
vamsi199 <vamsi.1991992@gmail.com>
2017-11-12 09:13:11 -0500

    added post request for form file

--- golang/web/server/post-request-body-types-TODO.go
+++ golang/web/server/post-request-body-types-TODO.go
@@ -9,6 +9,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"io"
+	"bytes"
+	"os"
 )
 
 func main() {
@@ -18,5 +21,31 @@ func main() {
 
 //1b) form data file type example
 func handlerFormDataFileType(w http.ResponseWriter, r *http.Request) {
+	var buf bytes.Buffer
+
+	file,header,err:=r.FormFile("hello")
+	if err!= nil {
+		fmt.Fprintln(w,"cannot get the file",err)
+	}
+	name:= header.Filename
+	_,err=io.Copy(&buf,file)
+	if err!= nil {
+		fmt.Fprintln(w,"cannot copy the file",err)
+	}
+	contents:=buf.String()
+	fmt.Println(contents)
+	_,err=os.Create(name)
+	if err!= nil {
+		fmt.Fprintln(w,"cannot create the file",err)
+	}
+	downfile,err:=os.OpenFile(name,os.O_WRONLY,os.ModeAppend)
+	if err!= nil {
+		fmt.Fprintln(w,"cannot open the file",err)
+	}
+	_,err=downfile.WriteString(contents)
+	if err!= nil {
+		fmt.Fprintln(w,"cannot write into the file",err)
+	}
+	defer file.Close()
 	fmt.Fprintln(w, "file received")
 }
