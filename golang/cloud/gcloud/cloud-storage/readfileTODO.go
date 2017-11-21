b4b9466b2b08ba2f4639c53b76491c0aa2f576c1
Srinivasa R Muly <msrinivasareddy@gmail.com>
2017-11-21 15:21:55 -0800

    Create readfileTODO.go

---
+++ golang/cloud/gcloud/cloud-storage/readfileTODO.go
@@ -0,0 +1,2 @@
+// example to demonstrate on how to connect to Google Cloud Storage and read a file
+//reference: https://cloud.google.com/appengine/docs/standard/go/googlecloudstorageclient/read-write-to-cloud-storage
