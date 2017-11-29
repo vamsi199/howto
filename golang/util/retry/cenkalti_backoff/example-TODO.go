0d2e82f50696d49f7002589b03d0fc5a2d45f84a
muly <msrinivasareddy@gmail.com>
2017-11-29 11:31:07 -0800

    wip example of a retry library

---
+++ golang/util/retry/cenkalti_backoff/example-TODO.go
@@ -0,0 +1,7 @@
+package main
+
+// based on the below blogpost
+// https://varunksaini.com/blog/backoff-retry-in-golang/
+// or the below cached copy
+// https://webcache.googleusercontent.com/search?q=cache:NJUoeIN_KV8J:https://varunksaini.com/blog/backoff-retry-in-golang/+&cd=3&hl=en&ct=clnk&gl=us
+
